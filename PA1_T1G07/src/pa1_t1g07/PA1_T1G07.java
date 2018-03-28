559e4d867f0c6bfe9b471cfa22b0d6cd2743e437
Francisco Lopes <franlopt@gmail.com>
2018-03-28 17:50:35 +0100

    Initial commit

---
+++ PA1_T1G07/src/pa1_t1g07/PA1_T1G07.java
@@ -0,0 +1,17 @@
+package pa1_t1g07;
+
+/**
+ *
+ * @author Francisco Lopes 76406
+ * @author Pedro Gusmão 77867
+ */
+public class PA1_T1G07 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
